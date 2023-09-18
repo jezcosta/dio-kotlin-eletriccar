c9d44ecaaa9b9fcc585a0151374b3b806bf8ecca
Jessica Costa <jessicacosta.dev@hotmail.com>
2023-09-17 22:50:45 -0300

    feat(calc): adding autonomy calculation

--- app/src/main/java/com/jezcosta/eletriccarapp/MainActivity.kt
+++ app/src/main/java/com/jezcosta/eletriccarapp/MainActivity.kt
@@ -2,10 +2,41 @@ package com.jezcosta.eletriccarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var price: EditText
+    lateinit var kmUsed: EditText
+    lateinit var btnCalculate: Button
+    lateinit var result: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+        setupListeners()
+    }
+
+    fun setupView() {
+        price = findViewById(R.id.et_price)
+        kmUsed = findViewById(R.id.et_km_used)
+        btnCalculate = findViewById(R.id.btn_calc)
+        result = findViewById(R.id.txt_result)
+    }
+
+    fun setupListeners () {
+        btnCalculate.setOnClickListener {
+            calculate()
+        }
+    }
+
+    fun calculate () {
+        val priceValue = price.text.toString().toFloat()
+        val kmUsedValue = kmUsed.text.toString().toFloat()
+
+        val resultado = priceValue / kmUsedValue
+        result.text = resultado.toString()
     }
 }
